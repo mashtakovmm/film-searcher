9ca33bc1fe0877eb76b3b4ca77048479bf62ff7d
Max <48150531+mashtakovmm@users.noreply.github.com>
2023-09-07 14:16:06 +0300

    update package

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../css/Home.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-
+    
 function Home() {
 
     return (
